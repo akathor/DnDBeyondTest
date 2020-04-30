14096b3eea0e1cf21bd058645da6c022a754c2e7
Erick Polato <erickmaster@gmail.com>
2020-04-29 19:21:28 -0500

    Estructura base con selenium imported

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='DnDBeyond Testing',
+      version='1.0',
+      description='DnDBeyond.com Automated Testing',
+      author='Erick Polato',
+      author_email='erickmaster@gmail.com',
+      url='https://github.com/akathor/DndBellondTesting',
+      packages=[
+            'Common'
+      ],
+     )
